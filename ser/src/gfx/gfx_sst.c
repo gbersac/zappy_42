554f143df3ea613c9af6c117586a20fbbd73d069
Guillaume Bersac <bersac_1@hotmail.fr>
2015-12-19 19:24:38 +0100

    ser: gfx_sst

--- ser/src/gfx/gfx_sst.c
+++ ser/src/gfx/gfx_sst.c
@@ -1,10 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gfx_sst.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbersac <gbersac@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/14 15:46:21 by gbersac           #+#    #+#             */
+/*   Updated: 2015/12/19 19:18:39 by gbersac          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cmd.h"
 
-int		gfx_sst(t_env *env, t_fd *fd, char *cmd)
+int     gfx_sst(t_env *env, t_fd *fd, char *cmd)
 {
-	return (1);
-	env = NULL;
-	cmd = NULL;
-	fd = NULL;
-}
+    char    *to_send;
 
+    sscanf(cmd, "sst %d\n", &env->map.time_d);
+    asprintf(&to_send, "sgt %d\n", env->map.time_d);
+    send_cmd_to_graphics(env, to_send);
+    free(to_send);
+    return (1);
+    fd = NULL;
+}
