42d5e82556a85ee95d0880d187c962d82bcba27f
Marion DEBELLEY <mdebelle@e2r8p20.42.fr>
2015-12-19 15:40:11 +0100

    cpp to c

---
+++ cli/src/connect.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   connect.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfrey <rfrey@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/05/14 20:39:34 by rfrey             #+#    #+#             */
+/*   Updated: 2014/06/04 22:01:31 by rfrey            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+extern "C" {
+	#include <stdio.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <netdb.h>
+	#include "libft.h"
+}
+
+#include "client.hpp"
+
+int		connect_to_server(char *ip, int port)
+{
+	int					sock;
+	struct protoent		*proto;
+	struct sockaddr_in	sin;
+
+	if (!(proto = getprotobyname("tcp")))
+		return (-1);
+	if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1)
+		return (-1);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = inet_addr(ip);
+	if (connect(sock, (const struct sockaddr *)&sin, sizeof(sin)))
+		return (-1);
+	return (sock);
+}
