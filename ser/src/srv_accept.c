5aba80d1d4372b573fa6f2702212f68ea75205df
Marion DEBELLEY <mdebelle@e1r12p23.42.fr>
2015-12-29 20:25:56 +0100

    ser: fix double accept line

--- ser/src/srv_accept.c
+++ ser/src/srv_accept.c
@@ -107,22 +107,6 @@ void		srv_accept(t_env *e, int s)
 	csin_len = sizeof(csin);
 	if ((cs = accept(s, (struct sockaddr*)&csin, &csin_len)) == -1)
 		ft_ferror("accept error");
-	
-	// char *message="BIENVENUE\n";
-	// send(cs,message,strlen(message),0);
-
-	// printf("srv_accept\n");
-
-
-	// int					cs;
-	// struct sockaddr_in	csin;
-	// socklen_t			csin_len;
-
-	// printf("graph\n");
-
-	csin_len = sizeof(csin);
-	if ((cs = accept(s, (struct sockaddr*)&csin, &csin_len)) == -1)
-		ft_ferror("accept error");
 
 	char *message="BIENVENUE\n";
 	send(cs,message,strlen(message),0);
